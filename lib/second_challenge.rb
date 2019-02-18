e3d7de87eca2c87d3d856da93c545a8caba85847
Tony Baker <this.tonybaker@gmail.com>
2019-02-18 20:51:57 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -9,7 +9,12 @@ def second_challenge
       },
     :capulet => {
       #your key/value pairs here!
-      }}
+      :patriarch => {},
+      :matriarch => {},
+      :hero => {},
+      :hero_friends => []
+      }
+    }
 
 
 end
